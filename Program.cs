d06b8b3a8e32570d06211cbe56d7359c454bc71c
User <User@DESKTOP-ESP2JT1>
2022-11-22 15:08:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._Numbers_Ending_in_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int index = 7; index <= 997; index+=10)
+            {
+                Console.WriteLine(index);
+            }
+        }
+    }
+}
